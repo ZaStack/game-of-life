956e67043aafe02de8c156febf91c3e15728315c
ZaStack <stackzpersonal@gmail.com>
2020-06-23 14:15:38 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,133 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback, useRef } from 'react';
+import produce from 'immer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const numRows = 50;
+const numCols = 50;
 
+const operations = [
+    [0, 1],
+    [0, -1],
+    [1, -1],
+    [-1, 1],
+    [1, 1],
+    [-1, -1],
+    [1, 0],
+    [-1, 0],
+];
+
+const generateEmptyGrid = () => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+        rows.push(Array.from(Array(numCols), () => 0));
+    }
+    return rows;
+};
+
+const App = () => {
+    const [grid, setGrid] = useState(() => {
+        return generateEmptyGrid();
+    });
+
+    const [running, setRunning] = useState(false);
+
+    const runningRef = useRef(running);
+    runningRef.current = running;
+
+    const runSimulation = useCallback(() => {
+        if (!runningRef.current) {
+            return;
+        }
+
+        setGrid((g) => {
+            return produce(g, (gridCopy) => {
+                for (let i = 0; i < numRows; i++) {
+                    for (let j = 0; j < numCols; j++) {
+                        let neighbors = 0;
+                        operations.forEach(([x, y]) => {
+                            const newI = i + x;
+                            const newJ = j + y;
+                            if (
+                                newI >= 0 &&
+                                newI < numRows &&
+                                newJ >= 0 &&
+                                newJ < numCols
+                            ) {
+                                neighbors += g[newI][newJ];
+                            }
+                        });
+
+                        if (neighbors < 2 || neighbors > 3) {
+                            gridCopy[i][j] = 0;
+                        } else if (g[i][j] === 0 && neighbors === 3) {
+                            gridCopy[i][j] = 1;
+                        }
+                    }
+                }
+            });
+        });
+        setTimeout(runSimulation, 100);
+    }, []);
+
+    return (
+        <>
+            <button
+                onClick={() => {
+                    setRunning(!running);
+                    if (!running) {
+                        runningRef.current = true;
+                        runSimulation();
+                    }
+                }}>
+                {running ? 'stop' : 'start'}
+            </button>
+            <button
+                onClick={() => {
+                    const rows = [];
+                    for (let i = 0; i < numRows; i++) {
+                        rows.push(
+                            Array.from(Array(numCols), () =>
+                                Math.random() > 0.7 ? 1 : 0
+                            )
+                        );
+                    }
+                    setGrid(rows);
+                }}>
+                random
+            </button>
+            <button
+                onClick={() => {
+                    setGrid(generateEmptyGrid());
+                }}>
+                clear
+            </button>
+            <div
+                style={{
+                    display: 'grid',
+                    gridTemplateColumns: `repeat(${numCols}, 20px)`,
+                }}>
+                {grid.map((rows, i) =>
+                    rows.map((col, j) => (
+                        <div
+                            key={`${i}-${j}`}
+                            onClick={() => {
+                                const newGrid = produce(grid, (gridCopy) => {
+                                    gridCopy[i][j] = grid[i][j] ? 0 : 1;
+                                });
+                                setGrid(newGrid);
+                            }}
+                            style={{
+                                width: 20,
+                                height: 20,
+                                backgroundColor: grid[i][j]
+                                    ? 'pink'
+                                    : undefined,
+                                border: 'solid 1px black',
+                            }}
+                        />
+                    ))
+                )}
+            </div>
+        </>
+    );
+};
 export default App;
