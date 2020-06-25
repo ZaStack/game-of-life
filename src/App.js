abe9bf4a82b1639aedab35352f9e7fdada96404c
ZaStack <stackzpersonal@gmail.com>
2020-06-25 16:12:01 -0700

    Partially styles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import randomColor from 'randomcolor';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import ButtonBar from './components/app-bar';
+import Typography from '@material-ui/core/Typography';
+
 //Set neighbor cells to check
 const operations = [
     [0, 1],
@@ -20,13 +22,18 @@ const useStyles = makeStyles((theme) => ({
     root: {
         display: 'flex',
         flexDirection: 'column',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
         flexWrap: 'wrap',
         '& > *': {
             margin: theme.spacing(1),
-            width: theme.spacing(16),
-            height: theme.spacing(16),
+            width: 'auto',
+            height: 'auto',
         },
     },
+    description: {
+        width: '50%'
+    }
 }));
 
 const App = () => {
@@ -38,20 +45,19 @@ const App = () => {
     const classes = useStyles();
     let runningCount = 0;
 
-
     const setSmall = () => {
         setNumRows(25);
-        setNumCols(40)
-    }
+        setNumCols(40);
+    };
     const setMedium = () => {
         setNumRows(40);
-        setNumCols(60)
-    }
+        setNumCols(60);
+    };
     const setLarge = () => {
         setNumRows(60);
-        setNumCols(80)
-    }
-    
+        setNumCols(80);
+    };
+
     // Generate the empty grid
 
     const generateEmptyGrid = () => {
@@ -124,143 +130,83 @@ const App = () => {
 
     return (
         <div className={classes.root}>
-            <ButtonBar
-                genCount={genCount}
-                setColor={setColor}
-                setRunning={() => {
-                    setRunning(!running);
-                    if (!running) {
-                        runningRef.current = true;
-                        runSimulation();
-                    }
-                }}
-                setSmall={setSmall}
-                setMedium={setMedium}
-                setLarge={setLarge}
-                seedGrid={seedGrid}
-                clearGrid={setGrid}
-                generateEmptyGrid={generateEmptyGrid}
-                running={running}
-            />
-            {/* <button
-                onClick={() => {
-                    setRunning(!running);
-                    if (!running) {
-                        runningRef.current = true;
-                        runSimulation();
-                    }
-                }}>
-                {running ? 'stop' : 'start'}
-            </button>
-            <button
-                onClick={() => {
-                    seedGrid();
-                }}>
-                seed
-            </button>
-            <button
-                onClick={() => {
-                    setGrid(generateEmptyGrid());
-                }}>
-                clear
-            </button> */}
-            {/* <button
-                onClick={() => {
-                    setNumRows(25);
-                    setNumCols(40);
-                }}>
-                small
-            </button>
-            <button
-                onClick={() => {
-                    setNumRows(40);
-                    setNumCols(60);
-                }}>
-                medium
-            </button>
-            <button
-                onClick={() => {
-                    setNumRows(60);
-                    setNumCols(80);
-                }}>
-                large
-            </button> */}
-            {/* <button
-                onClick={() => {
-                    setColor('red');
-                }}>
-                red
-            </button>
-            <button
-                onClick={() => {
-                    setColor('purple');
-                }}>
-                purple
-            </button>
-            <button
-                onClick={() => {
-                    setColor('green');
-                }}>
-                green
-            </button>
-            <button
-                onClick={() => {
-                    setColor('pink');
-                }}>
-                pink
-            </button>
-            <button
-                onClick={() => {
-                    setColor('orange');
-                }}>
-                orange
-            </button>
-            <button
-                onClick={() => {
-                    setColor('blue');
-                }}>
-                blue
-            </button>
-            <button
-                onClick={() => {
-                    setColor('yellow');
-                }}>
-                yellow
-            </button> */}
-
-            <Paper
-                style={{
-                    display: 'grid',
-                    gridTemplateColumns: `repeat(${numCols}, 20px)`,
-                }}>
-                {grid.map((rows, i) =>
-                    rows.map((col, j) => (
-                        <Paper
-                            elevation={3}
-                            key={`${i}-${j}`}
-                            onClick={() => {
-                                if (!running) {
-                                    const newGrid = produce(
-                                        grid,
-                                        (gridCopy) => {
-                                            gridCopy[i][j] = grid[i][j] ? 0 : 1;
-                                        }
-                                    );
-                                    setGrid(newGrid);
-                                }
-                            }}
-                            style={{
-                                width: 20,
-                                height: 20,
-                                backgroundColor: grid[i][j]
-                                    ? randomColor({ hue: `${color}` })
-                                    : 'black',
-                                border: 'solid 1px black',
-                            }}
-                        />
-                    ))
-                )}
-            </Paper>
+            <div className='container'>
+                <Paper
+                    style={{
+                        display: 'grid',
+                        gridTemplateColumns: `repeat(${numCols}, 20px)`,
+                    }}>
+                    {grid.map((rows, i) =>
+                        rows.map((col, j) => (
+                            <Paper
+                                elevation={3}
+                                key={`${i}-${j}`}
+                                onClick={() => {
+                                    if (!running) {
+                                        const newGrid = produce(
+                                            grid,
+                                            (gridCopy) => {
+                                                gridCopy[i][j] = grid[i][j]
+                                                    ? 0
+                                                    : 1;
+                                            }
+                                        );
+                                        setGrid(newGrid);
+                                    }
+                                }}
+                                style={{
+                                    width: 20,
+                                    height: 20,
+                                    backgroundColor: grid[i][j]
+                                        ? randomColor({ hue: `${color}` })
+                                        : 'black',
+                                    border: 'solid 1px black',
+                                }}
+                            />
+                        ))
+                    )}
+                </Paper>
+                <Paper className='app-bar-container'>
+                    <ButtonBar
+                        genCount={genCount}
+                        setColor={setColor}
+                        setRunning={() => {
+                            setRunning(!running);
+                            if (!running) {
+                                runningRef.current = true;
+                                runSimulation();
+                            }
+                        }}
+                        setSmall={setSmall}
+                        setMedium={setMedium}
+                        setLarge={setLarge}
+                        seedGrid={seedGrid}
+                        clearGrid={setGrid}
+                        generateEmptyGrid={generateEmptyGrid}
+                        running={running}
+                    />
+                </Paper>
+                <Typography className='description'>
+                    <ol className='description'>
+                        <li>
+                            Any live cell with fewer than two live neighbours
+                            dies, as if by underpopulation.
+                        </li>
+                        <li>
+                            Any live cell with two or three live neighbours
+                            lives on to the next generation.
+                        </li>
+                        <li>
+                            Any live cell with more than three live neighbours
+                            dies, as if by overpopulation.
+                        </li>
+                        <li>
+                            Any dead cell with exactly three live neighbours
+                            becomes a live cell, as if by reproduction.
+                        </li>
+                    </ol>
+                </Typography>
+            </div>
         </div>
     );
 };
