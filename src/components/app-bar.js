03ed453bd77ab08e42f3d71df0bd933badc999e4
ZaStack <stackzpersonal@gmail.com>
2020-06-26 10:24:43 -0700

    Finishes styling

--- src/components/app-bar.js
+++ src/components/app-bar.js
@@ -1,112 +1,121 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
-import Paper from '@material-ui/core/Paper';
-
+import { Paper, ButtonGroup, Select, FormControl, MenuItem, InputLabel, Button, Typography, Toolbar, AppBar, makeStyles } from '@material-ui/core'
 const useStyles = makeStyles((theme) => ({
     root: {
-        flexGrow: 1,
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginTop: '5px',
-        '& > *': {
-            margin: theme.spacing(1),
-        },
-        backgroundColor: '#0d0d0d',
-        width: '95%'
+        marginTop: '10px',
+        width: '80%',
+        backgroundColor: '#404040',
+        borderRadius: '20px',
+        
     },
-    // menuButton: {
-    //     marginRight: theme.spacing(2),
-    // },
+    appbar: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingBottom: '5px',
+        width: '100%',
+        backgroundColor: '#A49FB0',
+        borderRadius: '20px',
+    },
     title: {
+        display: 'flex',
+        justifyContent: 'center',
         flexGrow: 1,
-        maxWidth: '95%'
+        fontSize: '3rem',
+        color: '#404040'
     },
     formControl: {
         margin: theme.spacing(1),
         minWidth: 120,
+
     },
     selectEmpty: {
         marginTop: theme.spacing(2),
     },
+    toolbar: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '95%',
+        
+    },
+    selectionContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '5px'
+        
+    }
 }));
 
 const ButtonBar = (props) => {
     const classes = useStyles();
 
     return (
-        <Paper className={classes.root} elevation={3}>
-            <AppBar position='static'>
-                <Toolbar>
+        <Paper className={classes.root}>
+            <AppBar position='static' className={classes.appbar}>
+                <Toolbar className={classes.toolbar}>
                     <Typography variant='h6' className={classes.title}>
                         Generation: {props.genCount}
                     </Typography>
-                    <ButtonGroup
-                        color='#454545'
-                        aria-label='outlined primary button group'>
-                        <Button onClick={props.setRunning}>
-                            {props.running ? 'stop' : 'start'}
-                        </Button>
-                        <Button onClick={props.seedGrid}>Seed</Button>
-                        <Button
-                            onClick={() =>
-                                props.clearGrid(props.generateEmptyGrid)
-                            }>
-                            Clear
-                        </Button>
-                    </ButtonGroup>
-                    <FormControl className={classes.formControl}>
-                        <InputLabel id='size-select-label'>Size</InputLabel>
-                        <Select
-                            labelId='size-select-label'
-                            id='size-select'
-                            // value={props.size}
-                        >
-                            <MenuItem onClick={props.setSmall}>Small</MenuItem>
-                            <MenuItem onClick={props.setMedium}>
-                                Medium
-                            </MenuItem>
-                            <MenuItem onClick={props.setLarge}>Large</MenuItem>
-                        </Select>
-                    </FormControl>
-                    <FormControl className={classes.formControl}>
-                        <InputLabel id='color-select-label'>Color</InputLabel>
-                        <Select labelId='color-select-label' id='color-select'>
-                            <MenuItem onClick={() => props.setColor('red')}>
-                                Red
-                            </MenuItem>
-                            <MenuItem onClick={() => props.setColor('purple')}>
-                                Purple
-                            </MenuItem>
-                            <MenuItem onClick={() => props.setColor('green')}>
-                                Green
-                            </MenuItem>
-                            <MenuItem onClick={() => props.setColor('pink')}>
-                                Pink
-                            </MenuItem>
-                            <MenuItem onClick={() => props.setColor('orange')}>
-                                Orange
-                            </MenuItem>
-                            <MenuItem onClick={() => props.setColor('blue')}>
-                                Blue
-                            </MenuItem>
-                            <MenuItem onClick={() => props.setColor('yellow')}>
-                                Yellow
-                            </MenuItem>
-                        </Select>
-                    </FormControl>
+                    <div className={classes.selectionContainer}>
+                        <ButtonGroup
+                            color='#454545'
+                            aria-label='outlined primary button group'
+                            className={classes.buttongroup}>
+                            <Button onClick={props.setRunning}>
+                                {props.running ? 'stop' : 'start'}
+                            </Button>
+                            <Button onClick={props.seedGrid}>Seed</Button>
+                            <Button
+                                onClick={() =>
+                                    props.clearGrid(props.generateEmptyGrid)
+                                }>
+                                Clear
+                            </Button>
+                        </ButtonGroup>
+                        <FormControl className={classes.formControl}>
+                            <InputLabel id='size-select-label'>Size</InputLabel>
+                            <Select
+                                labelId='size-select-label'
+                                id='size-select'
+                                // value={props.size}
+                            >
+                                <MenuItem onClick={props.setSmall}>Small</MenuItem>
+                                <MenuItem onClick={props.setMedium}>
+                                    Medium
+                                </MenuItem>
+                                <MenuItem onClick={props.setLarge}>Large</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <FormControl className={classes.formControl}>
+                            <InputLabel id='color-select-label'>Color</InputLabel>
+                            <Select labelId='color-select-label' id='color-select'>
+                                <MenuItem onClick={() => props.setColor('red')}>
+                                    Red
+                                </MenuItem>
+                                <MenuItem onClick={() => props.setColor('purple')}>
+                                    Purple
+                                </MenuItem>
+                                <MenuItem onClick={() => props.setColor('green')}>
+                                    Green
+                                </MenuItem>
+                                <MenuItem onClick={() => props.setColor('pink')}>
+                                    Pink
+                                </MenuItem>
+                                <MenuItem onClick={() => props.setColor('orange')}>
+                                    Orange
+                                </MenuItem>
+                                <MenuItem onClick={() => props.setColor('blue')}>
+                                    Blue
+                                </MenuItem>
+                                <MenuItem onClick={() => props.setColor('yellow')}>
+                                    Yellow
+                                </MenuItem>
+                            </Select>
+                        </FormControl>
+                    </div>
                 </Toolbar>
             </AppBar>
         </Paper>
